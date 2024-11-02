466f253e301dc97a85f43b88e451879aeeccfa67
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-11-02 13:45:59 +0530

    Update common.mk

--- common.mk
+++ common.mk
@@ -431,7 +431,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
-    hardware/qcom-caf/common/libqti-perfd-client 
+    hardware/qcom-caf/common/libqti-perfd-client \
+    kernel/oneplus/sm8650 \
+    kernel/oneplus/sm8650-modules
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
