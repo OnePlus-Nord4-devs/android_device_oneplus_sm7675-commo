48ce097ba3f4c529c2716329069170e613b8e4c3
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-11-03 05:18:31 +0530

    Update common.mk

--- common.mk
+++ common.mk
@@ -432,8 +432,11 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
     hardware/qcom-caf/common/libqti-perfd-client \
-    kernel/oneplus/sm8650 \
-    kernel/oneplus/sm8650-modules
+
+# Kernel
+LOCAL_KERNEL := device/xiaomi/sunny-kernel/Image
+PRODUCT_COPY_FILES += \
+	$(LOCAL_KERNEL):kernel
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
