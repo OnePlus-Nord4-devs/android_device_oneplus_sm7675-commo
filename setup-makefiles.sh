a93799e144389fb43312c0133e01284210a1cc89
phhgsi <ajay24ce008@satiengg.in>
2024-11-13 07:17:48 +0530

    sm7675-common: avalonify setup-makefiles.sh
    Signed-off-by: phhgsi <ajay24ce008@satiengg.in>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "audi"
+write_headers "avalon"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
